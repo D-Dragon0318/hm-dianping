602d3a603b4f906ea9a722efcecb181fba56416d
spridra <2394527308@qq.com>
2023-12-21 10:13:06 +0800

    feat: 解决缓存穿透问题
    Signed-off-by: spridra <2394527308@qq.com>

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -15,8 +15,7 @@ import javax.annotation.Resource;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -38,24 +37,30 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         String shopJson = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
         //2.判断是否存在
         if (StrUtil.isNotBlank(shopJson)) {
+            //不为 null，不为空字符串：""，不为空格、全角空格、制表符、换行符，等不可见字符
+            //null代表缓存没有这个key-value，
+            //空字符串代表这个key-value存在，但是值为空也就是后面逻辑置空
             //3.存在，直接返回
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
-        } else {
-            //4.不存在，从数据库查询
-            Shop shop = getById(id);
-            //5.不存在，返回错误
-            if (shop == null) {
-                //不存在则存空
-//                stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, "");
-                return Result.fail("店铺不存在");
-            }
-            //6.将查询结果存入redis
-            stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
-            return Result.ok(shop);
         }
-
-
+        //这里往下要么是null要么是空字符串
+        //判断缓存中命中的是否是空值
+        if (shopJson != null){
+            // 返回一个错误信息
+            return Result.fail("店铺不存在");
+        }
+        //4.不存在，从数据库查询
+        Shop shop = getById(id);
+        //5.不存在，返回错误
+        if (shop == null) {
+            //不存在则存空
+            stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, "",CACHE_NULL_TTL,TimeUnit.MINUTES);
+            return Result.fail("店铺不存在");
+        }
+        //6.将查询结果存入redis
+        stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        return Result.ok(shop);
     }
 
     @Override
